a400e9483290b3710c88b72f9bb7736b3d9ce5b6
Box <bradycfox@gmail.com>
2018-10-24 14:17:02 -0400

    prevented mongoose.connect and app.list from executing when running tests

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
 });
 
 // Listen for incoming connections
-if (process.env.NODE_ENV !== 'test') {
+if (require.main === module) {
   mongoose
     .connect(
       MONGODB_URI,
