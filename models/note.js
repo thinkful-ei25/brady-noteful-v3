a2ddd754f1fb4da0f319a3957ec1face97805f37
Box <bradycfox@gmail.com>
2018-10-26 11:19:51 -0400

    tag schema

--- models/note.js
+++ models/note.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const noteSchema = mongoose.Schema({
   title: { type: String, required: true },
   content: String,
-  folderId: {type: mongoose.Schema.Types.ObjectId, ref: 'folder'}
+  folderId: {type: mongoose.Schema.Types.ObjectId, ref: 'Folder'}
 });
 
 noteSchema.set('timestamps', true);
